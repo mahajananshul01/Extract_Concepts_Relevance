94100cc33fe55c363aa58ff0bc185dea72fd2d0b
Anshul Mahajan <65218722+mahajananshul01@users.noreply.github.com>
2024-09-03 00:07:36 +0530

    Update For Debugging.py

--- For Debugging.py
+++ For Debugging.py
@@ -1,1 +1,24 @@
+# Filter the dataframe based on the date
+df_tmp = df_prod_metadata.loc[df_prod_metadata['PublishedDate'] >= past_month]
+
+# Check if df_tmp is not empty
+if len(df_tmp) > 0:
+    past_month_num_list = []
+    past_month_perc_list = []
+
+    for tag in concept_tags_unique:
+        num_with_tag = df_tmp.loc[df_tmp["Concept Tags"].str.contains(tag)].shape[0]
+        perc_with_tag = num_with_tag / len(df_tmp)
+
+        past_month_num_list.append(num_with_tag)
+        past_month_perc_list.append(str(round(perc_with_tag * 100)) + '%')
+else:
+    # Handle the case where there are no documents in the filtered dataframe
+    past_month_num_list = [0] * len(concept_tags_unique)
+    past_month_perc_list = ['0%'] * len(concept_tags_unique)
+
+output_table_1_df = pd.DataFrame()
+output_table_1_df["Concept"] = concept_tags_unique
+output_table_1_df["# of Docs this Month"] = past_month_num_list
+output_table_1_df["% of Docs this Month"] = past_month_perc_list
 
